396f6e07fce5967f9d6e2fe4b916bdec20e5ce43
Andgel <andgel.halley@epitech.eu>
2020-10-08 15:43:00 +0900

    Added icong

---
+++ src/Resources/version.hpp
@@ -0,0 +1,10 @@
+//
+// Created by andgel on 08/10/2020.
+//
+
+#ifndef UBC_VERSION_HPP
+#define UBC_VERSION_HPP
+
+#define UBC_VERSION_STRING "0.0.1"
+
+#endif //UBC_VERSION_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 //
 
 #include <iostream>
+#include "Resources/version.hpp"
 #include "Rendering/Screen.hpp"
 
 int main()
 {
+	sf::Image logo;
 	UntilBeingCrowned::Rendering::Screen screen{
-		"", 640, 480
+		"Until Being Crowned " UBC_VERSION_STRING, 640, 480
 	};
 	sf::Event event;
 
+	if (logo.loadFromFile("assets/icon.png"))
+		screen.setIcon(logo.getSize().x, logo.getSize().y, logo.getPixelsPtr());
 	while (screen.isOpen()) {
 		while (screen.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
